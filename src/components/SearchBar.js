ca190e65ed76c393daf1e59011a52bb8b8b9fcf4
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-18 18:47:25 -0700

    add video list

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,6 +9,7 @@ class SearchBar extends React.Component {
 
   onFormSubmit = event => {
     event.preventDefault();
+    this.props.onFormSubmit(this.state.term);
     //todo make sure we call callback from parent component
   };
   render() {
