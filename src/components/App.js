f75288ba2b534ebcf7352865d1cb0775119b1a29
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-23 16:13:42 -0700

    improve useability

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,21 @@ import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("buildings");
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = video => {
